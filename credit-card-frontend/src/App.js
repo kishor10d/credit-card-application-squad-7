d2b4693c268d480bc89428ba964509e0fb030d63
Kishor Mali <kishor10d@gmail.com>
2026-04-18 11:21:48 +0530

    Initial Commit

---
+++ credit-card-frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='col-md-6'>1</div>
+        <div className='col-md-6'>2</div>
+        <div className='col-md-6'>3</div>
+        <div className='col-md-6'>4</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
